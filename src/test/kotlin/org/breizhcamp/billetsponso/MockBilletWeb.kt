acdac81aa6d6be7feaea98a8ccce0a395eb5e339
Alexandre Thomazo <alex@thomazo.info>
2022-06-06 20:36:09 +0200

    Update dependencies + 2022

--- src/test/kotlin/org/breizhcamp/billetsponso/MockBilletWeb.kt
+++ src/test/kotlin/org/breizhcamp/billetsponso/MockBilletWeb.kt
@@ -3,7 +3,7 @@ package org.breizhcamp.billetsponso
 import spark.kotlin.Http
 import spark.kotlin.ignite
 
-fun main(args: Array<String>) {
+fun main() {
     val http: Http = ignite()
     http.port(9080)
             .threadPool(4, 2, 3600)
